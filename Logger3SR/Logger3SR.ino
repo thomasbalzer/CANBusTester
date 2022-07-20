e1b4dde9b3936ae7024e911172f94d2b1bbdddee
thomasbalzer <thomas.balzer16@gmail.com>
2022-07-20 13:37:06 -0600

    Update Logger3SR.ino

--- Logger3SR/Logger3SR.ino
+++ Logger3SR/Logger3SR.ino
@@ -23,10 +23,9 @@ bool toggle;
 bool newData = false;
 
 //Create a counter to keep track of message traffic
-uint32_t TXCount0 = 0;
 uint32_t TXCount1 = 0;
 uint32_t RXCount0 = 0;
-uint32_t RXCount1 = 0;
+
 
 //Define LED
 #define GREEN_LED_PIN 6
@@ -48,20 +47,20 @@ boolean BLUE_LED_state;
 #define BAUDRATE250K 250000
 
 void setup() {
-  
+
   // put your setup code here, to run once:
   //Set baudrate
-  Serial.begin(115200); delay(10000);
+  Serial.begin(115200); delay(400);
   Can1.begin();
   Can0.begin();
   Can1.setBaudRate(BAUDRATE250K);
   Can0.setBaudRate(BAUDRATE250K);
+  Can0.setMB((FLEXCAN_MAILBOX)0, RX, EXT);
+  Can0.enableMBInterrupts();
+  Can0.onReceive(MB0,canSniff);
+  Can0.mailboxStatus();
 
   //Set message extension, ID, and length
-  txmsg0.flags.extended = 1;
-  txmsg0.id = 0x100;
-  txmsg0.len = 8;
-
   txmsg1.flags.extended = 1;
   txmsg1.id = 0x101;
   txmsg1.len = 8;
@@ -81,11 +80,11 @@ void setup() {
   //Pull button high
   pinMode(button1, INPUT_PULLUP);
   pinMode(button2, INPUT_PULLUP);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Can0.events();
   buttonState1 = digitalRead(button1);
   buttonState2 = digitalRead(button2);
   unsigned long currentMillis = micros();
@@ -103,48 +102,11 @@ void loop() {
   }
 
   if (toggle) {
-    if (Can0.read(rxmsg0)) {
-      previousMillis = currentMillis;
-      newData = true;
-      printFrame(rxmsg0, 0, RXCount0++);
-      //RXCount0++;
-      //Toggle the LED
-      GREEN_LED_state = !GREEN_LED_state;
-      digitalWrite(GREEN_LED_PIN, GREEN_LED_state);
-    }
-    if (Can1.read(rxmsg1)) {
-      previousMillis = currentMillis;
-      newData = true;
-      printFrame(rxmsg1, 1, RXCount1++);
-      //RXCount1++;
-      //Toggle the LED
-      RED_LED_state = !RED_LED_state; 
-      digitalWrite(RED_LED_PIN, RED_LED_state);
-    }
-    while (!Can0.write(txmsg0));
-    TXTimer = 0;//Reset Timer
-
-    //Convert the 32-bit timestamp into 4 bytes with the most significant byte (MSB) first (Big endian).
-    uint32_t sysMicros = micros();
-    txmsg0.buf[0] = (sysMicros & 0xFF000000) >> 24;
-    txmsg0.buf[1] = (sysMicros & 0x00FF0000) >> 16;
-    txmsg0.buf[2] = (sysMicros & 0x0000FF00) >>  8;
-    txmsg0.buf[3] = (sysMicros & 0x000000FF);
-
-    //Convert the 32-bit transmit counter into 4 bytes with the most significant byte (MSB) first (Big endian).
-    txmsg0.buf[4] = (TXCount0 & 0xFF000000) >> 24;
-    txmsg0.buf[5] = (TXCount0 & 0x00FF0000) >> 16;
-    txmsg0.buf[6] = (TXCount0 & 0x0000FF00) >>  8;
-    txmsg0.buf[7] = (TXCount0 & 0x000000FF);
-
-    //Write the message on CAN channel 0
-    Can0.write(txmsg0);
-    TXCount0++;
-
     while (!Can1.write(txmsg1));
-    //Toggle the LED
-    BLUE_LED_state = !BLUE_LED_state;
-    digitalWrite(BLUE_LED_PIN, BLUE_LED_state);
+    uint32_t sysMicros = micros();
+    YELLOW_LED_state = !YELLOW_LED_state;
+    digitalWrite(YELLOW_LED_PIN, YELLOW_LED_state);
+
     //Serial.print("CAN0 Message Sent: ");
     //Serial.println(TXCount0);
     txmsg1.buf[0] = (sysMicros & 0xFF000000) >> 24;
@@ -161,34 +123,37 @@ void loop() {
     //Write the message on CAN channel 1
     Can1.write(txmsg1);
     TXCount1++;
-    //Toggle the LED
-    YELLOW_LED_state = !YELLOW_LED_state;
-    digitalWrite(YELLOW_LED_PIN, YELLOW_LED_state);
     //Serial.print("CAN1 Message Sent: ");
     //Serial.println(TXCount1);
   }
 
-  if (((currentMillis - previousMillis) >= 9000) && newData == true) {
-    previousMillis = currentMillis;
+  if (((currentMillis - previousMillis) >= 90000) && newData == true) {
     Serial.println(RXCount0);
-    Serial.println(RXCount1);
     RXCount0 = 0;
-    RXCount1 = 0;
     newData = false;
   }
 }
 
 
-  //A generic CAN Frame print function for the Serial terminal
-  void printFrame(CAN_message_t rxmsg, uint8_t channel, uint32_t RXCount)
-  {
-    char CANdataDisplay[50];
-    sprintf(CANdataDisplay, "%d %12lu %12lu %08X %d %d", channel, RXCount, micros(), rxmsg.id, rxmsg.flags.extended, rxmsg.len);
-    Serial.print(CANdataDisplay);
-    for (uint8_t i = 0; i < rxmsg.len; i++) {
-      char CANBytes[4];
-      sprintf(CANBytes, " %02X", rxmsg.buf[i]);
-      Serial.print(CANBytes);
-    }
-    Serial.println();
+//A generic CAN Frame print function for the Serial terminal
+void printFrame(CAN_message_t rxmsg, uint8_t channel, uint32_t RXCount)
+{
+  char CANdataDisplay[50];
+  sprintf(CANdataDisplay, "%d %12lu %12lu %08X %d %d", channel, RXCount, micros(), rxmsg.id, rxmsg.flags.extended, rxmsg.len);
+  Serial.print(CANdataDisplay);
+  for (uint8_t i = 0; i < rxmsg.len; i++) {
+    char CANBytes[4];
+    sprintf(CANBytes, " %02X", rxmsg.buf[i]);
+    Serial.print(CANBytes);
   }
+  Serial.println();
+}
+
+void canSniff(const CAN_message_t &rxmsg0) {
+  newData = true;
+  printFrame(rxmsg0, 0, RXCount0++);
+  //RXCount0++;
+  //Toggle the LED
+  GREEN_LED_state = !GREEN_LED_state;
+  digitalWrite(GREEN_LED_PIN, GREEN_LED_state);
+}
